86aedb126935acf482234eadd5f1f50dc0e478c6
Sam Medhurst <sammedhurst777@gmail.com>
2024-02-15 09:52:05 -0800

    Initializing repo on Github

---
+++ CHANGELOG.md
@@ -0,0 +1,57 @@
+### 2023-06-27
+* Fixed a bug that failed to prevent a `composer.lock` file from being committed to the repository. ([#103](https://github.com/pantheon-systems/wordpress-composer-managed/pull/103))
+* Removed the `upstream-require` script ([#105](https://github.com/pantheon-systems/wordpress-composer-managed/pull/105)). This is now available as a standalone Composer plugin: [`pantheon-systems/upstream-management`](https://packagist.org/packages/pantheon-systems/upstream-management)
+* Added a README to the Upstream Configuration path repository with notes about the new `upstream-management` package. ([#104](https://github.com/pantheon-systems/wordpress-composer-managed/pull/104))
+
+### 2023-05-23
+* Removes the `lh-hsts` plugin requirement from the `composer.json` file. ([#91](https://github.com/pantheon-systems/wordpress-composer-managed/pull/91))
+* Adds the [Pantheon WP Coding Standards](https://github.com/pantheon-systems/pantheon-wp-coding-standards) to use instead of the default PHPCS/WPCS standards ([#94](https://github.com/pantheon-systems/wordpress-composer-managed/pull/94))
+* Backports updates from Roots/Bedrock ([#90](https://github.com/pantheon-systems/wordpress-composer-managed/pull/90))
+* Updates the `post-install-cmd` hook to run a script that checks for the existence of symlinks before attempting to create them. ([#98](https://github.com/pantheon-systems/wordpress-composer-managed/pull/98))
+* Minor improvements to the Sage install process (included in [#98](https://github.com/pantheon-systems/wordpress-composer-managed/pull/98))
+
+### 2023-03-29
+* Changes the language in comments about appropriate usage of `config/application.php`. `application.php` should be used for any site config customizations rather than `wp-config.php` which should _not_ be edited. ([#76](https://github.com/pantheon-systems/wordpress-composer-managed/pull/76))
+* Updates the Sage install script to properly update the PHP version. ([#81](https://github.com/pantheon-systems/wordpress-composer-managed/pull/81))
+* Updates the Sage install script to force symbolic links rather than attepting to create them on each `composer install`. ([#79](https://github.com/pantheon-systems/wordpress-composer-managed/pull/79))
+* Updates the Sage install script to attempt to fix invalid theme names. ([#80](https://github.com/pantheon-systems/wordpress-composer-managed/pull/80))
+* Updates the Sage install script to fail hard if `jq` is not found, but provide links to download the executable. ([#82](https://github.com/pantheon-systems/wordpress-composer-managed/pull/82))
+* Fixes an issue that led to failing builds due to an unstaged `index.php` file created by the forced symlinks. ([#86](https://github.com/pantheon-systems/wordpress-composer-managed/pull/86))
+
+### 2023-02-02
+* Adds Composer Patches plugin ([#66](https://github.com/pantheon-systems/wordpress-composer-managed/pull/66))
+
+### 2023-01-11
+* Remove Dependabot ([#58](https://github.com/pantheon-systems/wordpress-composer-managed/pull/58))
+* Resolves issue where updates to commit choosing logic were causing a merge conflict. ([#60](https://github.com/pantheon-systems/wordpress-composer-managed/pull/60))
+* Adds a Roots Sage install script. See [docs/Installing-Sage.md](docs/Installing-Sage.md) ([#61](https://github.com/pantheon-systems/wordpress-composer-managed/pull/61))
+
+### 2022-11-30
+* Set minimum-stability to "stable" in `composer.json` ([#55](https://github.com/pantheon-systems/wordpress-composer-managed/pull/55))
+* Add Composer `post-install-cmd` to create symlinks to the `web/wp` directory (for better multisite support) ([#56](https://github.com/pantheon-systems/wordpress-composer-managed/pull/56))
+
+### 2022-11-09
+* Pull the latest versions of all packages ([#36](https://github.com/pantheon-systems/wordpress-composer-managed/pull/36))
+* Improved debug log path suggested in `.env.example` ([#38](https://github.com/pantheon-systems/wordpress-composer-managed/pull/38))
+* Get latest WP updates (and other WP-related dependencies) automatically ([#40](https://github.com/pantheon-systems/wordpress-composer-managed/pull/40))
+* Always pull the latest Pantheon plugins from wpackagist ([#45](https://github.com/pantheon-systems/wordpress-composer-managed/pull/45))
+* Change from `php_version` to `php-version` in testing matrix ([#46](https://github.com/pantheon-systems/wordpress-composer-managed/pull/46))
+* Additional Lando configuration ([#47](https://github.com/pantheon-systems/wordpress-composer-managed/pull/47))
+* Fix a typo in `README-internal.md` ([#48](https://github.com/pantheon-systems/wordpress-composer-managed/pull/48))
+
+### 2022-10-17
+* Load global .env file even if .env.local is absent ([#32](https://github.com/pantheon-systems/wordpress-composer-managed/pull/32))
+
+### 2022-10-14
+* Set permalink structure in build-env.install-cms Composer extra property ([#30](https://github.com/pantheon-systems/wordpress-composer-managed/pull/30))
+
+### 2022-10-03
+* Move Pantheon modifications in application.php ([#29](https://github.com/pantheon-systems/wordpress-composer-managed/pull/29))
+
+### 2022-09-23
+* `.gitignore` updates - Reported in the Pantheon Community Slack channel was the fact that the `.gitignore` included in this repository did not match the `.gitignore` (with commonly-ignored files) in the default [WordPress upstream](https://github.com/pantheon-systems/wordpress) ([#24](https://github.com/pantheon-systems/wordpress-composer-managed/pull/24))
+* Use `.env.local` for local development ([#26](https://github.com/pantheon-systems/wordpress-composer-managed/pull/26)) - Fixes an issue where Lando was not using `.env` files locally.
+* README updates ([#27](https://github.com/pantheon-systems/wordpress-composer-managed/pull/27) and [#28](https://github.com/pantheon-systems/wordpress-composer-managed/pull/28)) - Adds link to https://github.com/pantheon-upstreams/wordpress-composer-managed for forking, as well as additional guidance about the default branch name for custom upstreams.
+
+### 2022-06-16
+* Initial fork from [`roots/bedrock`](https://roots.io/bedrock) at 1.20.0. See the changelog prior to that at [`github.com/roots/bedrock`](https://github.com/roots/bedrock/blob/97f7826f3d284b82d83ff15d13bfc22628d660e2/CHANGELOG.md)
---
+++ upstream-configuration/scripts/ComposerScripts.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * @file
+ * Contains \WordPressComposerManaged\ComposerScripts.
+ *
+ * Custom Composer scripts and implementations of Composer hooks.
+ */
+
+namespace WordPressComposerManaged;
+
+use Composer\Script\Event;
+
+class ComposerScripts
+{
+   /**
+    * Prepare for Composer to update dependencies.
+    */
+    public static function preUpdate(Event $event)
+    {
+    }
+
+    /**
+     * postUpdate
+     *
+     * After "composer update" runs, we have the opportunity to do additional
+     * fixups to the project files.
+     *
+     * @param Composer\Script\Event $event
+     *   The Event object passed in from Composer
+     */
+    public static function postUpdate(Event $event)
+    {
+        // for future use
+    }
+
+    /**
+     * Apply composer.json Updates
+     *
+     * During the Composer pre-update hook, check to see if there are any
+     * updates that need to be made to the composer.json file. We cannot simply
+     * change the composer.json file in the upstream, because doing so would
+     * result in many merge conflicts.
+     */
+    public static function applyComposerJsonUpdates(Event $event)
+    {
+        $io = $event->getIO();
+
+        $composerJsonContents = file_get_contents("composer.json");
+        $composerJson = json_decode($composerJsonContents, true);
+        $originalComposerJson = $composerJson;
+
+        // Check to see if the platform PHP version (which should be major.minor.patch)
+        // is the same as the Pantheon PHP version (which is only major.minor).
+        // If they do not match, force an update to the platform PHP version. If they
+        // have the same major.minor version, then
+        $platformPhpVersion = static::getCurrentPlatformPhp($event);
+        $pantheonPhpVersion = static::getPantheonPhpVersion($event);
+        $updatedPlatformPhpVersion = static::bestPhpPatchVersion($pantheonPhpVersion);
+        if ((substr($platformPhpVersion, 0, strlen($pantheonPhpVersion)) != $pantheonPhpVersion) && !empty($updatedPlatformPhpVersion)) {
+            $io->write("<info>Setting platform.php from '$platformPhpVersion' to '$updatedPlatformPhpVersion' to conform to pantheon php version.</info>");
+            $composerJson['config']['platform']['php'] = $updatedPlatformPhpVersion;
+        }
+
+        // add our post-update-cmd hook if it's not already present
+        $our_hook = 'WordPressComposerManaged\\ComposerScripts::postUpdate';
+        // if does not exist, add as an empty arry
+        if (! isset($composerJson['scripts']['post-update-cmd'])) {
+            $composerJson['scripts']['post-update-cmd'] = [];
+        }
+
+        // if exists and is a string, convert to a single-item array
+        if (is_string($composerJson['scripts']['post-update-cmd'])) {
+            $composerJson['scripts']['post-update-cmd'] = [$composerJson['scripts']['post-update-cmd']];
+        }
+
+        // if exists and is an array and does not contain our hook, add our hook (again, only the last check is needed)
+        if (! in_array($our_hook, $composerJson['scripts']['post-update-cmd'])) {
+            // We're making our other changes if and only if we're already adding our hook
+            // so that we don't overwrite customer's changes if they undo these changes.
+            // We don't want customers to remove our hook, so it will be re-added if they remove it.
+            $io->write("<info>Adding post-update-cmd hook to composer.json</info>");
+            $composerJson['scripts']['post-update-cmd'][] = $our_hook;
+
+            // Remove our upstream convenience scripts, if the user has not removed them.
+            if (isset($composerJson['scripts']['upstream-require'])) {
+                unset($composerJson['scripts']['upstream-require']);
+            }
+            // Also remove it from the scripts-descriptions section.
+            if (isset($composerJson['scripts-descriptions']['upstream-require'])) {
+                unset($composerJson['scripts-descriptions']['upstream-require']);
+            }
+            // Now, if scripts-descriptions is empty, remove it. This prevents an issue where it's re-encoded as an array instead of an (empty) object.
+            if (empty($composerJson['scripts-descriptions'])) {
+                unset($composerJson['scripts-descriptions']);
+            }
+        }
+
+        if (serialize($composerJson) == serialize($originalComposerJson)) {
+            return;
+        }
+
+        // Write the updated composer.json file
+        $composerJsonContents = static::jsonEncodePretty($composerJson);
+        file_put_contents("composer.json", $composerJsonContents . PHP_EOL);
+    }
+
+    /**
+     * jsonEncodePretty
+     *
+     * Convert a nested array into a pretty-printed json-encoded string.
+     *
+     * @param array $data
+     *   The data array to encode
+     * @return string
+     *   The pretty-printed encoded string version of the supplied data.
+     */
+    public static function jsonEncodePretty(array $data)
+    {
+        $prettyContents = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        $prettyContents = preg_replace('#": \[\s*("[^"]*")\s*\]#m', '": [\1]', $prettyContents);
+        return $prettyContents;
+    }
+
+    /**
+     * Get current platform.php value.
+     */
+    private static function getCurrentPlatformPhp(Event $event)
+    {
+        $composer = $event->getComposer();
+        $config = $composer->getConfig();
+        $platform = $config->get('platform') ?: [];
+        if (isset($platform['php'])) {
+            return $platform['php'];
+        }
+        return null;
+    }
+
+    /**
+     * Get the PHP version from pantheon.yml or pantheon.upstream.yml file.
+     */
+    private static function getPantheonConfigPhpVersion($path)
+    {
+        if (!file_exists($path)) {
+            return null;
+        }
+
+        if (preg_match('/^php_version:\s?(\d+\.\d+)$/m', file_get_contents($path), $matches)) {
+            return $matches[1];
+        }
+    }
+
+    /**
+     * Get the PHP version from pantheon.yml.
+     */
+    private static function getPantheonPhpVersion(Event $event)
+    {
+        $composer = $event->getComposer();
+        $config = $composer->getConfig();
+        $pantheonYmlPath = dirname($config->get('vendor-dir')) . '/pantheon.yml';
+        $pantheonUpstreamYmlPath = dirname($config->get('vendor-dir')) . '/pantheon.upstream.yml';
+
+        if ($pantheonYmlVersion = static::getPantheonConfigPhpVersion($pantheonYmlPath)) {
+            return $pantheonYmlVersion;
+        } elseif ($pantheonUpstreamYmlVersion = static::getPantheonConfigPhpVersion($pantheonUpstreamYmlPath)) {
+            return $pantheonUpstreamYmlVersion;
+        }
+        return null;
+    }
+
+    /**
+     * Determine which patch version to use when the user changes their platform php version.
+     */
+    private static function bestPhpPatchVersion($pantheonPhpVersion)
+    {
+        // Integrated Composer requires PHP 7.1 at a minimum.
+        $patchVersions = [
+            '8.2' => '8.2.0',
+            '8.1' => '8.1.13',
+            '8.0' => '8.0.26',
+            '7.4' => '7.4.33',
+            '7.3' => '7.3.33',
+            '7.2' => '7.2.34',
+            '7.1' => '7.1.33',
+        ];
+        if (isset($patchVersions[$pantheonPhpVersion])) {
+            return $patchVersions[$pantheonPhpVersion];
+        }
+        // This feature is disabled if the user selects an unsupported php version.
+        return '';
+    }
+
+    // This is called by a Circle CI build step to modify the gitignore file and
+    // add any required dependencies to the repository that is pushed to Pantheon.
+    public static function prepareForPantheon()
+    {
+      // Fix up .gitignore: remove everything above the "::: cut :::" line
+      $gitignoreFile = getcwd() . '/.gitignore';
+      $gitignoreContents = file_get_contents($gitignoreFile);
+      $gitignoreContents = preg_replace('/.*::: cut :::*/s', '', $gitignoreContents);
+      file_put_contents($gitignoreFile, $gitignoreContents);
+    }
+
+
+}
